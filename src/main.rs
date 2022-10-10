af84813ae51ded1994263f58257ef2df55e83745
Insaf <59477383+Act0r1@users.noreply.github.com>
2022-10-10 16:10:14 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use std::sync::RwLock;
-
 fn main() {
-    let xs = RwLock::new([0, 0, 0, 0]);
-
-    crossbeam::scope(|scope| {
-        for _ in 0..10 {
-            scope.spawn(|_| {
-                let mut guard = xs.write().unwrap();
-                let xs: &mut [i32; 4] = &mut guard;
-                for x in xs {
-                    *x += 1;
-                }
-            });
-        }
-    })
-    .unwrap();
-
-    println!("xs : {:?}", xs);
+    println!("Hello world");
 }
